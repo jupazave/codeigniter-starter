c3bf716bdc8ae5cf694840b09db9248b6253e019
Dominik Biedebach <dominik.biedebach@gmail.com>
2015-04-10 16:22:19 +0200

    Removed unnecessary stuff

--- src/application/helpers/bleech_helper.php
+++ src/application/helpers/bleech_helper.php
@@ -1,13 +1,9 @@
-<?php
+cs<?php
   function asset_url() {
     return base_url().'assets/';
   }
 
   function load_asset($asset) {
     return asset_url().$asset;
-  }
-
-  function get_template_part($part) {
-    return 'application/views/templates/'.$part.'.php';
   }
 ?>
